d5bff2b3e9bfb8939eb70f3848f46d7478dcd4fa
Kurniawan Wijaya <kurniawan.wijaya@gmail.com>
2020-04-09 09:40:21 +1000

    refactor 144

--- solutions/144.js
+++ solutions/144.js
@@ -9,67 +9,32 @@
  *     this.left = this.right = null;
  * }
  */
-
-/**
- * Definition for a node property state (i.e. if the property in question has been processed).
- */
-function NodePropertyState() {
-    this.val = this.left = this.right = false;
-}
-
-/**
- * @param {TreeNode} node
- * @return {boolean}
- */
-var isValidNode = function(node) {
-    return (node !== null && typeof node !== "undefined" && typeof node.val === "number");
-};
-
 /**
  * @param {TreeNode} root
  * @return {number[]}
  */
 var preorderTraversal = function(root) {
-    let result = [];
+    var stack = [];
+    var output = [];
+    var currentNode = null;
     
-    let currentNode = root;
-    let currentNodePropertyState = new NodePropertyState();
-    let processedNodeStack = [];
-    let processedNodePropertyStateStack = [];
+    if (root !== null) {
+        stack.push(root);
+    }
     
-    while (true) {
-        if (isValidNode(currentNode)) {
-            // process current node
-            if (!currentNodePropertyState.val) {
-                result.push(currentNode.val);
-                currentNodePropertyState.val = true;
-            }
-            
-            if (isValidNode(currentNode.left) && !currentNodePropertyState.left) {
-                // traverse to the left node
-                currentNodePropertyState.left = true;
-                processedNodeStack.push(currentNode);
-                processedNodePropertyStateStack.push(currentNodePropertyState);
-                
-                currentNode = currentNode.left;
-                currentNodePropertyState = new NodePropertyState();
-            } else if (isValidNode(currentNode.right) && !currentNodePropertyState.right) {
-                // traverse to the right node
-                currentNodePropertyState.right = true;
-                processedNodeStack.push(currentNode);
-                processedNodePropertyStateStack.push(currentNodePropertyState);
-                
-                currentNode = currentNode.right;
-                currentNodePropertyState = new NodePropertyState();
-            } else {
-                // traverse back to the parent node
-                currentNode = processedNodeStack.pop();
-                currentNodePropertyState = processedNodePropertyStateStack.pop();
-            }
-        } else {
-            break;
+    while (stack.length > 0) {
+        // process current node
+        currentNode = stack.pop();
+        output.push(currentNode.val);
+        
+        // add children to stack for further processing
+        if (currentNode.right !== null) {
+            stack.push(currentNode.right);
+        }
+        if (currentNode.left !== null) {
+            stack.push(currentNode.left);
         }
     }
     
-    return result;
+    return output;
 };
